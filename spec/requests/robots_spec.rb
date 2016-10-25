075945289a4595eec8055e8994615e84a81ab805
dblock <dblock@dblock.org>
2016-10-25 14:20:49 -0400

    Fix #3: undefined method call for nil:NilClass.

---
+++ spec/requests/robots_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'Robots.txt', type: :request do
+  include Rack::Test::Methods
+
+  def app
+    SlackRubyBotServer::Api::Middleware.instance
+  end
+
+  it 'can be retrieved' do
+    get '/robots.txt'
+    expect(last_response.status).to eq 200
+    expect(last_response.body).to eq ['User-Agent: *', 'Disallow: /'].join("\n")
+  end
+end
